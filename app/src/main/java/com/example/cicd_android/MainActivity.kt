5b74819ffa1b6d2f144c799f9d2397491b5fe366
Boris <boris88210@gmail.com>
2023-08-18 16:23:09 +0800

    - 修改 build.gradle 使用buildConfigField抓取參數

--- app/src/main/java/com/example/cicd_android/MainActivity.kt
+++ app/src/main/java/com/example/cicd_android/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.cicd_android
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.tv_hello)
+        tv.text = BuildConfig.inputFromJenkins
     }
 }
